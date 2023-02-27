4d0dfa125e4979c8340a1439fdc9eb70fdc43661
Oishika <oc618@nyu.edu>
2023-02-26 23:12:13 -0800

    Was able to get the delete to work

---
+++ Data/SpotifyUserDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Pluto.Data
+{
+    public class SpotifyUser
+    {
+        private string username { get; set; }
+        private string password { get; set; }
+    }
+}
+
